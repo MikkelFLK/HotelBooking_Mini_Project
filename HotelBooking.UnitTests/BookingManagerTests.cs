ad443f2b1d51c81efa3d43bde9ee479ae3196842
ThaCaper <43811142+ThaCaper@users.noreply.github.com>
2021-09-22 12:19:43 +0200

    Update BookingManagerTests.cs
    AB#11

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -1,33 +1,50 @@
 using System;
+using System.Collections.Generic;
 using HotelBooking.Core;
+using HotelBooking.Infrastructure.Repositories;
 using HotelBooking.UnitTests.Fakes;
+using Moq;
 using Xunit;
 
 namespace HotelBooking.UnitTests
 {
     public class BookingManagerTests
     {
-        private IBookingManager bookingManager;
+        private BookingManager bookingManager;
+        private Mock<IRepository<Booking>> fakeBookingRepo;
+        private Mock<IRepository<Room>> fakeRoomRepo;
+
 
         public BookingManagerTests(){
             DateTime start = DateTime.Today.AddDays(10);
             DateTime end = DateTime.Today.AddDays(20);
-            IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
-            IRepository<Room> roomRepository = new FakeRoomRepository();
-            bookingManager = new BookingManager(bookingRepository, roomRepository);
+            
+            fakeBookingRepo = new Mock<IRepository<Booking>>();
+            fakeRoomRepo = new Mock<IRepository<Room>>();
+            bookingManager = new BookingManager(fakeBookingRepo.Object, fakeRoomRepo.Object);
         }
 
         [Fact]
         public void FindAvailableRoom_StartDateNotInTheFuture_ThrowsArgumentException()
         {
-            // Arrange
-            DateTime date = DateTime.Today;
-
-            // Act
-            Action act = () => bookingManager.FindAvailableRoom(date, date);
+            
+            //Arrange
+            DateTime date = DateTime.Today.AddDays(-1);
 
             // Assert
-            Assert.Throws<ArgumentException>(act);
+            Assert.Throws<ArgumentException>(() => bookingManager.FindAvailableRoom(date, date));
+
+        }
+
+        [Fact]
+        public void FindAvailableRoom_StartDateOnToday_ThrowsArgumentException()
+        {
+            //Arrange
+            DateTime date = DateTime.Today;
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => bookingManager.FindAvailableRoom(date, date));
+
         }
 
         [Fact]
